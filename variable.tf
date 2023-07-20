c81995eec2239a4a5842547006a00d55e76554ff
Nidhya <Nidhyamails@gmail.com>
2023-07-19 23:32:38 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,35 @@
+variable "default_tags" {
+  type        = map(string)
+  description = "Map of default tags to apply to resources"
+  default = {
+    Project = "nodejs"
+  }
+}
+
+variable "region" {
+  type        = string
+  description = "The region to deploy resources to"
+  default     = "ap-south-1"
+}
+# VPC Variables
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for VPC"
+  default     = "10.0.0.0/16"
+}
+#Subnet cidr
+variable "private_cidr" {
+  type        = string
+  description = "CIDR block for Private subnet"
+  default     = "10.0.0.0/20"
+}
+variable "ec2_key_pair" {
+  type        = string
+  description = "EC2 key pair"
+  default     = "iasmumbai"
+}
+variable "database_private_ip" {
+  type        = string
+  description = "Private ip address of database"
+  default     = "10.0.0.100"
+}
